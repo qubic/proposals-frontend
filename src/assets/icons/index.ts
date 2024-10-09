1e9cd82ea9e216e933ad9e36909b2d6d8f49daa9
alexmf91 <alexmunoz91@gmail.com>
2024-10-09 19:31:16 +0200

    feat: add new SVG icons for check, arrows, and alerts

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,11 @@
+export { default as CheckCircleIcon } from './check-circle.svg?react'
+export { default as CheckIcon } from './check.svg?react'
+export { default as CopyTextIcon } from './copy-text.svg?react'
+export { default as ExclamationCircleIcon } from './exclamation-circle.svg?react'
+export { default as ExclamationTriangleIcon } from './exclamation-triangle.svg?react'
+export { default as GlobeGrayIcon } from './globe-gray.svg?react'
+export { default as Infocon } from './info.svg?react'
+export { default as XmarkIcon } from './xmark.svg?react'
+
+export * from './arrows'
+export * from './logo'
