d628076a4c8fc9421016c128e1b4088395874631
alexmf91 <alexmunoz91@gmail.com>
2024-10-15 23:47:11 +0200

    feat: add ProposalType enum

---
+++ src/types/enums.ts
@@ -0,0 +1,7 @@
+// eslint-disable-next-line import/prefer-default-export
+export enum ProposalType {
+  GENERAL_PROPOSAL = 'GeneralProposal',
+  COMPUTOR_DONATION = 'ComputorDonation',
+  CCF_FUNDING_REQUEST = 'CCFFundingRequest',
+  SHAREHOLDER_PROPOSAL = 'ShareholderProposal'
+}
