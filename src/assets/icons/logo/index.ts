ef983c41954ca7e2c49d07a7ffe41f69fd4a51cc
alexmf91 <alexmunoz91@gmail.com>
2024-10-18 18:52:44 +0200

    feat: add new lock, unlock and qubic connect icons, and update icon exports

--- src/assets/icons/logo/index.ts
+++ src/assets/icons/logo/index.ts
@@ -1,2 +1,3 @@
 export { default as QubicProposalsWhiteLogo } from './logo-text-on-dark.svg?react'
 export { default as QubicLogoShort } from './logo-text-short.svg?react'
+export { default as QubicConnectLogo } from './qubic-connect.svg?react'
