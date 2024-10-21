2166ef9f03d2139e72bb17cdfb084889ac4ab602
alexmf91 <alexmunoz91@gmail.com>
2024-10-21 23:30:30 +0200

    feat: add QubicIcon SVG and export it in the logo index

--- src/assets/icons/logo/index.ts
+++ src/assets/icons/logo/index.ts
@@ -1,3 +1,4 @@
 export { default as QubicProposalsWhiteLogo } from './logo-text-on-dark.svg?react'
 export { default as QubicLogoShort } from './logo-text-short.svg?react'
+export { default as QubicIcon } from './logo.svg?react'
 export { default as QubicConnectLogo } from './qubic-connect.svg?react'
