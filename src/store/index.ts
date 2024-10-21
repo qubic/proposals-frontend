e6572d3c342ce26e225d325d025109f768ef6583
alexmf91 <alexmunoz91@gmail.com>
2024-10-21 23:21:38 +0200

    feat: add modal slice for managing modal state in the store

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit'
 import { qubicRpcApi } from './apis/qubic-rpc.api'
 import localeReducer from './localeSlice'
+import modalReducer from './modalSlice'
 
 export const store = configureStore({
   reducer: {
     locale: localeReducer,
+    modal: modalReducer,
     [qubicRpcApi.reducerPath]: qubicRpcApi.reducer
   },
 
