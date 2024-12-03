66dca23b6c35498ebc8f73f4d93434c70d9ea335
alexmf91 <alexmunoz91@gmail.com>
2024-12-03 23:16:51 +0100

    refactor: simplify useTailwindBreakpoint hook and improve breakpoint handling

--- src/hooks/useTailwindBreakpoint.tsx
+++ src/hooks/useTailwindBreakpoint.tsx
@@ -1,73 +1,48 @@
 import { theme } from '@app/theme'
-import debounce from 'debounce'
 import { useCallback, useEffect, useMemo, useState } from 'react'
 
-type Breakpoints = keyof typeof theme.breakpoints
+type Breakpoints = keyof typeof theme.breakpoints | null
 
 type Output = {
   activeBreakpoint: Breakpoints
   isMobile: boolean
 }
 
-// Helper function to create MediaQueryLists
-const createMediaQueryLists = (): Record<string, MediaQueryList> => {
-  return Object.entries(theme.breakpoints).reduce(
-    (acc, [key, value]) => {
-      const updatedAcc = { ...acc }
-      if (typeof value === 'string') {
-        updatedAcc[key] = window.matchMedia(`(min-width: ${value})`)
-      } else if (value.raw) {
-        updatedAcc[key] = window.matchMedia(value.raw)
-      }
-      return updatedAcc
-    },
-    {} as Record<string, MediaQueryList>
-  )
-}
+const getMediaQueryLists = Object.entries(theme.breakpoints).reduce(
+  (acc, [key, value]) => {
+    const updatedAcc = { ...acc }
+    if (typeof value === 'string') {
+      updatedAcc[key] = window.matchMedia(`(min-width: ${value})`)
+    } else if (value.raw) {
+      updatedAcc[key] = window.matchMedia(value.raw)
+    }
+    return updatedAcc
+  },
+  {} as Record<string, MediaQueryList>
+)
 
 export default function useTailwindBreakpoint(): Output {
-  const [mediaQueryLists, setMediaQueryLists] =
-    useState<Record<string, MediaQueryList>>(createMediaQueryLists())
-
-  const getCurrentBreakpoint = useCallback((): Breakpoints => {
-    return (
-      (Object.entries(mediaQueryLists)
-        .filter(([, mql]) => mql.matches)
-        .map(([key]) => key)
-        .pop() as Breakpoints) || 'xxs'
-    )
-  }, [mediaQueryLists])
-
-  const [activeBreakpoint, setActiveBreakpoint] = useState<Breakpoints>(() =>
-    getCurrentBreakpoint()
-  )
+  const [activeBreakpoint, setActiveBreakpoint] = useState<Breakpoints>(null)
 
   const isMobile = useMemo(
     () => !activeBreakpoint || activeBreakpoint === 'xxs' || activeBreakpoint === 'xs',
     [activeBreakpoint]
   )
 
-  const handleResize = useMemo(
-    () =>
-      debounce(() => {
-        setActiveBreakpoint(getCurrentBreakpoint())
-      }, 100),
-    [getCurrentBreakpoint]
-  )
+  const handleResize = useCallback(() => {
+    const matchedBreakpoint = Object.entries(getMediaQueryLists)
+      .filter((mqlEntry) => mqlEntry[1].matches)
+      .map(([key]) => key)
+      .pop()
+
+    setActiveBreakpoint((matchedBreakpoint as Breakpoints) || null)
+  }, [])
 
   useEffect(() => {
-    const updateMediaQueryLists = () => {
-      setMediaQueryLists(createMediaQueryLists())
-    }
-
-    // Update MediaQueryLists on component mount and on orientation changes
+    handleResize() // Check on initial load
     window.addEventListener('resize', handleResize)
-    window.addEventListener('orientationchange', updateMediaQueryLists)
 
-    return () => {
-      window.removeEventListener('resize', handleResize)
-      window.removeEventListener('orientationchange', updateMediaQueryLists)
-    }
+    return () => window.removeEventListener('resize', handleResize)
   }, [handleResize])
 
   return { activeBreakpoint, isMobile }
